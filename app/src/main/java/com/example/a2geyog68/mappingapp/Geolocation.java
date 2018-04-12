13d7a07eb309b152324c12ca7bf1997f5a08ec6c
Gisylia <2geyog68@solent.ac.uk>
2018-04-12 09:15:49 +0100

    location and gps

---
+++ app/src/main/java/com/example/a2geyog68/mappingapp/Geolocation.java
@@ -0,0 +1,16 @@
+package com.example.a2geyog68.mappingapp;
+
+
+
+
+
+
+
+
+
+/**
+ * Created by user on 09/04/2018.
+ */
+
+public class Geolocation {
+}
--- app/src/main/java/com/example/a2geyog68/mappingapp/MainActivity.java
+++ app/src/main/java/com/example/a2geyog68/mappingapp/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.a2geyog68.mappingapp;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,23 +16,36 @@ import org.osmdroid.views.MapView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+
 
 import java.nio.BufferUnderflowException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
     MapView mv;
 
 
     @Override
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
+    /**this is called when the main activity is first created*/
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**this is for the location manager */
+        LocationManager mgr= (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,this);
+// if you want to test indoors change gps_provider to network_provider (everything in caps)
+
+        /**this is for the preference menu*/
         Configuration.getInstance().load
                 (this,PreferenceManager.getDefaultSharedPreferences (this));
 
-
+        /**this is for the map view*/
         setContentView(R.layout.activity_main);
 
         mv = (MapView) findViewById(R.id.map1);
@@ -37,16 +54,10 @@ public class MainActivity extends AppCompatActivity {
         mv.getController().setZoom(16);
 
         mv.getController().setCenter(new GeoPoint(51.05, -0.72));
-
-
     }
 
 
-
-
     // adding a new method to inflate the string XML
-
-
     public boolean onCreateOptionsMenu (Menu menu)
 
     {
@@ -57,9 +68,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /** choosing the different map views*/
     public boolean onOptionsItemSelected (MenuItem item)
-
-
     {
 
         if (item.getItemId() == R.id.choosemap)
@@ -77,18 +87,13 @@ public class MainActivity extends AppCompatActivity {
 
     {
         if (resultCode == RESULT_OK)
-
         {
-
             if (requestCode == 0) //mapChooseActivity returning it's intent
-
             {
-
                 Bundle bundle = returnIntent.getExtras();
                 boolean hikebike = bundle.getBoolean("com.example.gisylia_g.mappingapp.hikebike");
 
                 if (hikebike == true)
-
                 {
                     mv.setTileSource(TileSourceFactory.HIKEBIKEMAP);
                 }
@@ -96,12 +101,49 @@ public class MainActivity extends AppCompatActivity {
                 else
                 {
                     mv.setTileSource(TileSourceFactory.MAPNIK);
-
                 }
-
             }
-
         }
+    }
+
+/** when the location changes */
+    public void onLocationChanged(Location newLoc) // new lock represents your current gps location
+    {
+        Toast.makeText
+                (this, "Location= " +
+                        newLoc.getLatitude()+ "" +
+                        newLoc.getLongitude(), Toast.LENGTH_LONG).show();
+
+        mv.getController().setCenter(new GeoPoint( newLoc.getLatitude() ,newLoc.getLongitude() ));
+
+    }
+
+    /** when the location is disabled*/
+    public void onProviderDisabled(String provider)
+   {
+        Toast.makeText(this, "Provider" +
+                        provider + "disabled",
+                        Toast.LENGTH_LONG).show();
+   }
+
+
+    /**when the location is enabled */
+    public void onProviderEnabled (String provider)
+    {
+        Toast.makeText(this, "Provider" +
+                        provider + "enabled",
+                Toast.LENGTH_LONG).show();
+    }
+
+    /**when the location changes a status update*/
+    public void onStatusChanged(String provider, int status, Bundle extras)
+    {
+        Toast.makeText(this, "Status changed:" +
+                        status,
+                Toast.LENGTH_LONG).show();
+
+
+
 
     }
 
